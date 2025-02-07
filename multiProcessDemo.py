def3dea927c53e8cb144944b0d7f1eca3e6f0328
DESKTOP-FQJFF8E\cdr <dkrkdlqh@hanmail.net>
2025-02-07 17:09:35 +0900

    Revert "no message"
    This reverts commit 9a7aaa495c1fbe4d0248af5e4113063167bb30dc.

--- multiProcessDemo.py
+++
@@ -1,7 +0,0 @@
-from mainController import MainController
-from multiProcessController import MultiProcessController
-
-if __name__ == "__main__" :
-
-    # mainController = MainController()
-    userProcessController = MultiProcessController()
--- singleProcessDemo.py
+++
@@ -1,7 +0,0 @@
-from mainController import MainController
-from singleProcessController import SingleProcessController
-
-if __name__ == "__main__" :
-
-    # mainController = MainController()
-    userProcessController = SingleProcessController()
